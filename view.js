194f2320803f824fe263548543c672495e08f1ae
assafs <assafs@wix.com>
2017-07-26 14:09:44 +0300

    pubsub refactoring

--- view.js
+++ view.js
@@ -7,41 +7,6 @@
   let eventHandlers = {};
   const rootElm = document.querySelector('#root');
 
-  function unsubscribeAll() {
-    eventHandlers = {};
-  }
-
-  function subscribe(eventId, func, isWindow) {
-    if (!eventHandlers[eventId]) {
-      eventHandlers[eventId] = [];
-    }
-
-    eventHandlers[eventId].push({func: func});
-
-    if(isWindow){
-      window.addEventListener(eventId, func);
-    } else {
-      rootElm.addEventListener(eventId, publish);
-    }
-  }
-
-  function publish(event) {
-    const eventId = event.type;
-    const closestAncestor = event.target.closest('li') || event.target;
-    const args = { dataId: closestAncestor.getAttribute('data-id'),
-                   dataType: event.target.getAttribute('data-type')}
-    if (!eventHandlers[eventId]) {
-      return false;
-    }
-
-    let subscribers = eventHandlers[eventId],
-      len = subscribers ? subscribers.length : 0;
-
-    while (len--) {
-      subscribers[len].func(args);
-    }
-  };
-
   function renderRoot(uiState) {
     rootElm.innerHTML = `
       <div class="content-container">
@@ -73,52 +38,52 @@
   function renderStocksHeader(isFilterOn) {
     const headerElm = document.querySelector('.header');
     headerElm.innerHTML += `<span>stokr</span>
-          <ul class="header-buttons">
-            <li>
-              <a href="#search"> 
-                <button class="icon-search header-icon"></button>
-              </a>
-            </li>
-            <li>
-              <button class="icon-refresh header-icon"></button>
-            </li>
-            <li>
-              <button data-type="filter-toggle-btn" class="icon-filter header-icon ${isFilterOn ? 'green' : ''}"></button>
-             </li>
-            <li>
-              <button class="icon-settings header-icon"></button>
-            </li>
-          </ul>`
+      <ul class="header-buttons">
+        <li>
+          <a href="#search"> 
+            <button class="icon-search header-icon"></button>
+          </a>
+        </li>
+        <li>
+          <button class="icon-refresh header-icon"></button>
+        </li>
+        <li>
+          <button data-type="filter-toggle-btn" class="icon-filter header-icon ${isFilterOn ? 'green' : ''}"></button>
+         </li>
+        <li>
+          <button class="icon-settings header-icon"></button>
+        </li>
+      </ul>`
   }
 
   function renderFilterPanel(isFilterOn) {
     const mainElm = document.querySelector('.main');
     mainElm.innerHTML += `
-        <div class="filter-panel flex-sb-center ${isFilterOn? 'visible' : 'hidden'}">
-          <ul class="filter-list">
-            <li class="filter-item">
-              <label for="name">By Name</label> 
-              <input type="text" id="name" class="filter-input">
-            </li>
-            <li class="filter-item">
-              <label for="range-from">By Range: From</label>
-              <input type="text" id="range-from" class="filter-input">
-            </li>
-            <li class="filter-item">
-              <label for="gane">By Gain</label>
-              <select id="gane" class="filter-input"> 
-                <option value="all" selected>All</option> 
-                <option value="losing">Losing</option>
-                <option value="gaining">Gaining</option>
-              </select>
-            </li>
-            <li class="filter-item">
-              <label for="range-to">By Range: To</label> 
-              <input type="text" id="range-to" class="filter-input">
-            </li>
-          </ul>
-          <button class="btn-filter-apply">Apply</button>
-        </div>`
+      <div class="filter-panel flex-sb-center ${isFilterOn? 'visible' : 'hidden'}">
+        <ul class="filter-list">
+          <li class="filter-item">
+            <label for="name">By Name</label> 
+            <input type="text" id="name" class="filter-input">
+          </li>
+          <li class="filter-item">
+            <label for="range-from">By Range: From</label>
+            <input type="text" id="range-from" class="filter-input">
+          </li>
+          <li class="filter-item">
+            <label for="gane">By Gain</label>
+            <select id="gane" class="filter-input"> 
+              <option value="all" selected>All</option> 
+              <option value="losing">Losing</option>
+              <option value="gaining">Gaining</option>
+            </select>
+          </li>
+          <li class="filter-item">
+            <label for="range-to">By Range: To</label> 
+            <input type="text" id="range-to" class="filter-input">
+          </li>
+        </ul>
+        <button class="btn-filter-apply">Apply</button>
+      </div>`
   }
 
   function renderStocks(stocks, isFilterOn) {
@@ -133,29 +98,34 @@
     const disabledUp = index === 0 ? 'disabled' : '';
     const disabledDown = index === stocks.length - 1 ? 'disabled' : '';
 
-    return `<li data-id="${ stock.Symbol }" class="stock-item flex-sb-center">
-            <span>${ stock.Symbol } (${ stock.Name })</span>
-            <div class="stock-right  flex-sb-center">
-              <span>${ Number(stock.LastTradePriceOnly).toFixed(2) }</span>
-              <button class='btn-stock  ${getButtonColor(stock)}' data-type="stock-change-btn">
-${ stockChange }</button>
-              <div class="up-down-arrows ${isFilterOn ? 'hidden' : ''}">
-                <button data-type="arrow-up-btn" class="icon-arrow arrow-up" 
-${disabledUp}></button>
-                <button data-type="arrow-down-btn" class="icon-arrow arrow-down"
-${disabledDown}></button>
-              </div>
-            </div>
-          </li>`;
+    return `
+    <li data-id="${ stock.Symbol }" class="stock-item flex-sb-center">
+      <span>${ stock.Symbol } (${ stock.Name })</span>
+      <div class="stock-right  flex-sb-center">
+        <span>${ Number(stock.LastTradePriceOnly).toFixed(2) }</span>
+        <button class='btn-stock  ${getButtonColor(stock)}' data-type="stock-change-btn">${ stockChange }</button>
+        <div class="up-down-arrows ${isFilterOn ? 'hidden' : ''}">
+          <button data-type="arrow-up-btn" class="icon-arrow arrow-up" ${disabledUp}></button>
+          <button data-type="arrow-down-btn" class="icon-arrow arrow-down" ${disabledDown}></button>
+        </div>
+      </div>
+    </li>`;
   }
 
   function getButtonColor(stock) {
     return stock.change.startsWith('-') ? 'btn-red' : 'btn-green';
   }
 
+  rootElm.addEventListener('click', (e) => {
+    const closestAncestor = event.target.closest('li') || event.target;
+    const eventData = {
+      dataId: closestAncestor.getAttribute('data-id'),
+      dataType: event.target.getAttribute('data-type')
+    };
+    window.STKR.PubSub.publish('view-clicked', eventData );
+  });
+  
   window.STKR.View = {
-    subscribe,
-    unsubscribeAll,
     renderRoot
   };
 
